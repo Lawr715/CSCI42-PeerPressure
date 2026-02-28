1e8ce34e9542216ddbcd9ca96944253ed2cbc247
SantosKurtMatthew <kurtsantos924@gmail.com>
2026-02-28 15:37:36 +0800

    Initialized project and Pomodoro page

---
+++ app/Pomodoro/button.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
---
+++ app/Pomodoro/page.tsx
@@ -0,0 +1,12 @@
+import { MyButton } from "./button"
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-sky-400">
+    <div>Pomodoro Timer</div>
+    <MyButton />
+    </div>
+  );
+}
