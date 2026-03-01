54874a5e8fbe4a9e65c92868723a783c142b5920
SantosKurtMatthew <kurtsantos924@gmail.com>
2026-03-01 10:52:23 +0800

    Created Register, Login, and Profile pages with basic login functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
-import Image from "next/image";
-
+"use client";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
-    <div>Hello World</div>
+    <main className="flex items-center justify-center h-screen bg-neutral-950 text-white">
+      <div className="flex gap-4">
+        <button
+          onClick={() => router.push("/sign-up")}
+          className="bg-white text-black font-medium px-6 py-2 rounded-md hover:bg-gray-200"
+        >
+          Sign Up
+        </button>
+        <button
+          onClick={() => router.push("/sign-in")}
+          className="border border-white text-white font-medium px-6 py-2 rounded-md hover:bg-neutral-800"
+        >
+          Sign In
+        </button>
+      </div>
+      <div>Hello World</div>
+    </main>
   );
 }
 
