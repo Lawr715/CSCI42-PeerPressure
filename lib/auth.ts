122a17e474ee476539fb00b3acce69221a1efc30
Lawrence Paul Cado Marino <lawrencemarino18@gmail.com>
2026-04-16 00:09:29 +0800

    invalid origin v4

--- lib/auth.ts
+++ lib/auth.ts
@@ -27,6 +27,8 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL,
     emailAndPassword: { 
         enabled: true 
     },
